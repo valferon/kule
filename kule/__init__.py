7dde2153e63f84e2c3251849f1f320c386af9a23
fatiherikli <fatiherikli@gmail.com>
2013-04-02 22:42:12 +0300

    Restructured bottle app

--- kule/__init__.py
+++ kule/__init__.py
@@ -9,71 +9,75 @@ from bottle import Bottle, route, run, request, response, abort, error
 
 connection = Connection().dbpatterns
 
-kule = Bottle()
 
+class Kule(object):
+    def __init__(self):
+        pass
 
-@kule.route('/:collection/:pk', method='GET')
-def get_document(collection, pk):
-    cursor = connection[collection]
-    return jsonify(cursor.find_one({
-        "_id": ObjectId(pk)
-    }) or abort(404))
+    def get_document(self, collection, pk):
+        cursor = connection[collection]
+        return jsonify(cursor.find_one({
+            "_id": ObjectId(pk)
+        }) or abort(404))
 
+    def replace_document(self, collection, pk):
+        connection[collection].update({"_id": ObjectId(pk)},
+                                      request.json)
+        return jsonify(request.json)
 
-@kule.route('/:collection/:pk', method='PUT')
-def replace_document(collection, pk):
-    connection[collection].update({"_id": ObjectId(pk)},
-                                  request.json)
-    return jsonify(request.json)
+    def update_document(self, collection, pk):
+        connection[collection].update({"_id": ObjectId(pk)},
+                                      {"$set": request.json})
+        response.status = 202
+        return get_document(collection, pk)
 
+    def remove_document(self, collection, pk):
+        connection[collection].remove({"_id": ObjectId(pk)})
+        response.status = 204
 
-@kule.route('/:collection/:pk', method='PATCH')
-def update_document(collection, pk):
-    connection[collection].update({"_id": ObjectId(pk)},
-                                  {"$set": request.json})
-    response.status = 202
-    return get_document(collection, pk)
+    def post_collection(self, collection):
+        inserted = connection[collection].insert(request.json)
+        response.status = 201
+        return jsonify({"_id": inserted})
 
+    def get_collection(self, collection):
+        limit = int_or_default(request.query.limit, 20)
+        offset = int_or_default(request.query.offset, 0)
+        cursor = connection[collection].find()
 
-@kule.route('/:collection/:pk', method='DELETE')
-def get_document(collection, pk):
-    connection[collection].remove({"_id": ObjectId(pk)})
-    response.status = 204
+        meta = {
+            "limit": limit,
+            "offset": offset,
+            "total_count": cursor.count(),
+        }
 
+        objects = cursor.skip(offset).limit(limit)
 
-@kule.route('/:collection', method='GET')
-def get_collection(collection):
-    limit = int_or_default(request.query.limit, 20)
-    offset = int_or_default(request.query.offset, 0)
-    cursor = connection[collection].find()
+        return jsonify({"meta": meta,
+                        "objects": list(objects)})
 
-    meta = {
-        "limit": limit,
-        "offset": offset,
-        "total_count": cursor.count(),
-    }
+    def error(self, code, message, *args):
+        return jsonify({"error": code, "message": message})
 
-    objects = cursor.skip(offset).limit(limit)
+    def get_error_handler(self):
+        return {
+            500: partial(self.error, 500, "Internal Server Error"),
+            404: partial(self.error, 404, "Not Found"),
+        }
 
-    return jsonify({"meta": meta,
-                    "objects": list(objects)})
+    def get_app(self):
+        app = Bottle()
+        app.route('/:collection/:pk', method='GET')(self.get_document)
+        app.route('/:collection/:pk', method='PUT')(self.replace_document)
+        app.route('/:collection/:pk', method='PATCH')(self.update_document)
+        app.route('/:collection/:pk', method='DELETE')(self.remove_document)
+        app.route('/:collection', method='GET')(self.get_collection)
+        app.route('/:collection', method='POST')(self.post_collection)
+        app.error_handler = self.get_error_handler()
+        return app
 
-
-@kule.route('/:collection', method='POST')
-def post_collection(collection):
-    inserted = connection[collection].insert(request.json)
-    response.status = 201
-    return jsonify({"_id": inserted})
-
-
-def error(code, message, *args):
-    return jsonify({"error": code, "message": message})
-
-
-kule.error_handler = {
-    500: partial(error, 500, "Internal Server Error"),
-    404: partial(error, 404, "Not Found"),
-}
 
 if __name__ == "__main__":
-    run(app=kule, host='localhost', port=8000, debug=True, reloader=True)
+    kule = Kule()
+    run(app=kule.get_app(), host='localhost', port=8000,
+        debug=True, reloader=True)
