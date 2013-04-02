6662f3ee2228d21dfd82e4cd1c8814fc08ef681b
fatiherikli <fatiherikli@gmail.com>
2013-04-03 00:33:27 +0300

    Added bundler methods

--- kule/__init__.py
+++ kule/__init__.py
@@ -9,42 +9,54 @@ from bottle import Bottle, route, run, request, response, abort, error
 
 
 class Kule(object):
-    def __init__(self, database, host=None, port=None):
+    def __init__(self, database=None, host=None, port=None,
+                 allowed_collections=None):
         self.connection = self.connect(database, host, port)
+        self.allowed_collections = None
 
     def connect(self, database, host=None, port=None):
         return Connection(host=host, port=port)[database]
 
-    def get_document(self, collection, pk):
-        cursor = self.connection[collection]
-        return jsonify(cursor.find_one({
-            "_id": ObjectId(pk)
-        }) or abort(404))
+    def get_collection(self, collection):
+        if self.allowed_collections and collection \
+                not in self.allowed_collections:
+            abort(403)
+        return self.connection[collection]
 
-    def replace_document(self, collection, pk):
-        self.connection[collection].update({"_id": ObjectId(pk)},
-                                           request.json)
+    def get_detail(self, collection, pk):
+        cursor = self.get_collection(collection)
+        data = cursor.find_one({"_id": ObjectId(pk)}) or abort(404)
+        return jsonify(self.get_bundler(collection)(data))
+
+    def put_detail(self, collection, pk):
+        collection = self.get_collection(collection)
+        collection.update({"_id": ObjectId(pk)},
+                          request.json)
         return jsonify(request.json)
 
-    def update_document(self, collection, pk):
-        self.connection[collection].update({"_id": ObjectId(pk)},
-                                           {"$set": request.json})
+    def patch_detail(self, collection, pk):
+        collection = self.get_collection(collection)
+        collection.update({"_id": ObjectId(pk)},
+                          {"$set": request.json})
         response.status = 202
         return get_document(collection, pk)
 
-    def remove_document(self, collection, pk):
-        self.connection[collection].remove({"_id": ObjectId(pk)})
+    def delete_detail(self, collection, pk):
+        collection = self.get_collection(collection)
+        collection.remove({"_id": ObjectId(pk)})
         response.status = 204
 
-    def post_collection(self, collection):
-        inserted = self.connection[collection].insert(request.json)
+    def post_list(self, collection):
+        collection = self.get_collection(collection)
+        collection.insert(request.json)
         response.status = 201
         return jsonify({"_id": inserted})
 
-    def get_collection(self, collection):
+    def get_list(self, collection):
+        collection = self.get_collection(collection)
         limit = int_or_default(request.query.limit, 20)
         offset = int_or_default(request.query.offset, 0)
-        cursor = self.connection[collection].find()
+        cursor = collection.find()
 
         meta = {
             "limit": limit,
@@ -53,32 +65,49 @@ class Kule(object):
         }
 
         objects = cursor.skip(offset).limit(limit)
+        objects = map(self.get_bundler(collection), objects)
 
         return jsonify({"meta": meta,
-                        "objects": list(objects)})
+                        "objects": objects})
 
-    def error(self, code, message, *args):
-        return jsonify({"error": code, "message": message})
+    def get_bundler(self, collection):
+        return getattr(self, "build_%s_bundle" % collection.name,
+                       self.build_bundle)
+
+    def build_bundle(self, data):
+        return data
 
     def get_error_handler(self):
         return {
-            500: partial(self.error, 500, "Internal Server Error"),
-            404: partial(self.error, 404, "Not Found"),
+            500: partial(self.error, "Internal Server Error."),
+            404: partial(self.error, "Document Not Found."),
+            501: partial(self.error, "Not Implemented."),
+            405: partial(self.error, "Method Not Allowed."),
+            403: partial(self.error, "Forbidden."),
         }
 
-    def get_app(self):
-        app = Bottle()
-        app.route('/:collection/:pk', method='GET')(self.get_document)
-        app.route('/:collection/:pk', method='PUT')(self.replace_document)
-        app.route('/:collection/:pk', method='PATCH')(self.update_document)
-        app.route('/:collection/:pk', method='DELETE')(self.remove_document)
-        app.route('/:collection', method='GET')(self.get_collection)
-        app.route('/:collection', method='POST')(self.post_collection)
-        app.error_handler = self.get_error_handler()
-        return app
+    def dispatch_views(self):
+        for method in ("get", "post", "put", "patch", "delete"):
+            self.app.route('/:collection', method=method)(
+                getattr(self, "%s_list" % method, self.not_implemented))
+            self.app.route('/:collection/:pk', method=method)(
+                getattr(self, "%s_detail" % method, self.not_implemented))
+
+    def get_bottle_app(self):
+        self.app = Bottle()
+        self.dispatch_views()
+        self.app.error_handler = self.get_error_handler()
+        return self.app
+
+    def not_implemented(self, *args, **kwargs):
+        abort(501)
+
+    def error(self, message, error):
+        return jsonify({"error": error.status_code,
+                        "message": message})
 
 
 if __name__ == "__main__":
     kule = Kule(database="selam")
-    run(app=kule.get_app(), host='localhost', port=8000,
+    run(app=kule.get_bottle_app(), host='localhost', port=8003,
         debug=True, reloader=True)
