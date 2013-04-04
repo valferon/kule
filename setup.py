766614ef85a78545788bcd4d2695b7cd26d83729
fatiherikli <fatiherikli@gmail.com>
2013-04-04 23:37:31 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='Kule',
+      version='0.1',
+      description='REST Interface for MongoDB',
+      author='Fatih Erikli',
+      author_email='fatiherikli@gmail.com',
+      url='https://github.com/fatiherikli/kule',
+      packages=['kule'],
+      install_requires = [
+          'bottle==0.11.6',
+          'pymongo==2.5',
+      ]
+)
