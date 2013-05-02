c6be087258eb53ea7b80b47d7fc061c42ae76600
fatiherikli <fatiherikli@gmail.com>
2013-05-02 03:58:44 +0300

    Renamed authenticate endpoint as sessions

--- kule/contrib/auth.py
+++ kule/contrib/auth.py
@@ -1,11 +1,15 @@
 import hashlib
 import uuid
 
-from kule import Kule, jsonify, request, abort
+import bottle
+
+from kule import Kule, jsonify, request, response, abort
 
 
 class KuleWithAuth(Kule):
-
+    """
+    Adds authentication endpoints to kule
+    """
     def authenticate(self):
         collection = self.connection['users']
         username = request.json.get('username')
@@ -16,13 +20,13 @@ class KuleWithAuth(Kule):
             'username': username,
             'password': hasher.hexdigest(),
         }) or abort(400, 'Wrong username or password')
-        access_token = uuid.uuid4()
+        access_token = str(uuid.uuid4())
         self.connection['access_tokens'].insert({
             'access_token': access_token,
             'user_id': user.get('id')})
-        return jsonify({
-            'access_token': access_token
-        })
+        user.pop('password')
+        user.update({'access_token': access_token})
+        return jsonify(user)
 
     def register(self):
         collection = self.connection['users']
@@ -31,23 +35,24 @@ class KuleWithAuth(Kule):
         email = request.json.get('email')
         if not username or not password:
             abort(400, 'Please give an username and password')
-        if self.collection.find_one({'username': username}):
+        if collection.find_one({'username': username}):
             abort(400, 'A user with that username already exists')
-        if self.collection.find_one({'email': email}):
+        if collection.find_one({'email': email}):
             abort(400, 'A user with that email already exists')
         hasher = hashlib.md5()
         hasher.update(password)
-        created = collection.insert({
+        response.status = 201
+        return jsonify({"_id": collection.insert({
             'username': request.json.get('username'),
             'email': request.json.get('email'),
             'password': hasher.hexdigest()
-        })
-        return jsonify({"_id": created})
+        })})
 
     def dispatch_views(self):
         super(KuleWithAuth, self).dispatch_views()
-        self.app.route('/authenticate', method='post')(self.authenticate)
-        self.app.route('/users', method='post')(self.register)
-
+        self.app.route('/sessions', method='post')(self.authenticate)
+        self.app.route('/users', method=['post'])(self.register)
+        self.app.route('/sessions', method='options')(self.empty_response)
+        self.app.route('/users', method='options')(self.empty_response)
 
 kule = KuleWithAuth
